e087637f2521f6fd6468ac1e5c070a77db55c457
ravindrapv <ravindra.ravi4849@gmail.com>
2022-07-04 14:52:41 +0530

    middleware

--- block-BNaabs/sample/server.js
+++ block-BNaabs/sample/server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser')
 
 var app = express();
 
-
+app.use(express.urlencoded({extended: false}))
 app.use(logger("dev"));
 app.use(cookieParser());
 
@@ -16,22 +16,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/form', (req,res) => {
+app.get('/new', (req,res) => {
     res.sendFile(__dirname + "/form.html");
-    console.log(req.query);
-    res.send(req.query);
 })
 
-app.get('/new/:username', (req,res) => {
+app.get('/user/:username', (req,res) => {
     var username = req.params.username;
     res.send(username);
 })
-app.post('/form', (req,res) => {
-    res.sendFile(__dirname + "/form.html");
-    console.log(req.query);
-    res.send(req.query);
+app.post('/new', (req,res) => {
+    res.json(req.body);
 })
 
-app.listen(4000, () => {
-  console.log("server is listing on port 4k");
+app.listen(3000, () => {
+  console.log("server is listing on port 3k");
 });
